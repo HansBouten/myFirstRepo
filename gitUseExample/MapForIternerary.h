c1c20441825755a5b056cf7c0547db9f38010351
Hans Bouten <hans.bouten@home.nl>
2013-10-29 11:33:56 +0100

    Start with map feature

---
+++ gitUseExample/MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  gitUseExample
+//
+//  Created by Hans Bouten on 29-10-13.
+//  Copyright (c) 2013 Bentis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+@end
---
+++ gitUseExample/MapForIternerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.m
+//  gitUseExample
+//
+//  Created by Hans Bouten on 29-10-13.
+//  Copyright (c) 2013 Bentis. All rights reserved.
+//
+
+#import "MapForIternerary.h"
+
+@implementation MapForIternerary
+
+@end
